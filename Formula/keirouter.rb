bac761dd2cb42d70d55ba1492e16ff4e464873c4
ozan-fn <152951901+ozan-fn@users.noreply.github.com>
2026-07-15 06:23:55 +0000

    chore: update Homebrew formula for v0.1.28

--- Formula/keirouter.rb
+++ Formula/keirouter.rb
@@ -1,27 +1,27 @@
-# Auto-updated by release.yml on tag v0.1.27. Do not edit manually.
+# Auto-updated by release.yml on tag v0.1.28. Do not edit manually.
 class Keirouter < Formula
   desc "AI API router — unified gateway for 20+ LLM providers with fallback, caching, and dashboard"
   homepage "https://github.com/mydisha/keirouter"
-  version "0.1.27"
+  version "0.1.28"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mydisha/keirouter/releases/download/v0.1.27/keirouter_v0.1.27_darwin_arm64.tar.gz"
-      sha256 "66d57dc23af380a53ecac638c9874e5155f75e4dcac33f34b28fbb69c1ec3bec"
+      url "https://github.com/mydisha/keirouter/releases/download/v0.1.28/keirouter_v0.1.28_darwin_arm64.tar.gz"
+      sha256 "ee79ad480316b9bac0b7ae4d1fce0cf020752c16574d1c76222af3c6692ba18c"
     else
-      url "https://github.com/mydisha/keirouter/releases/download/v0.1.27/keirouter_v0.1.27_darwin_amd64.tar.gz"
-      sha256 "b587a3cd5d2ca249abea1c14907672309fe127901e412c0a8eeb2339626b3980"
+      url "https://github.com/mydisha/keirouter/releases/download/v0.1.28/keirouter_v0.1.28_darwin_amd64.tar.gz"
+      sha256 "7468e5334160c9a324a6b55a60419393c9d39223f9ede23a33150e0d6abeea32"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/mydisha/keirouter/releases/download/v0.1.27/keirouter_v0.1.27_linux_arm64.tar.gz"
-      sha256 "c4b2100e715d1680cdf8c99f2b4431423c58d8e8538b4b5edc4a0c5889d77b03"
+      url "https://github.com/mydisha/keirouter/releases/download/v0.1.28/keirouter_v0.1.28_linux_arm64.tar.gz"
+      sha256 "6241546c8e225e0bbccbab6f386ac09589e8b9f3a0ba93d8e8e1c58b64f57991"
     else
-      url "https://github.com/mydisha/keirouter/releases/download/v0.1.27/keirouter_v0.1.27_linux_amd64.tar.gz"
-      sha256 "c46795831b56322b0194cd1ff76fe355d6961cf323112cc2386c43151f852fac"
+      url "https://github.com/mydisha/keirouter/releases/download/v0.1.28/keirouter_v0.1.28_linux_amd64.tar.gz"
+      sha256 "c2c0786775d970674a495c62c2aafd0120816d7dd02e206ac82748f7ba9bdd13"
     end
   end
 
